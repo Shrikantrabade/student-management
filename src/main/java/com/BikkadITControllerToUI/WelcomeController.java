cbc079cdefe451a55863c2fab7d762b083bc308a
Shrikant Rabade <shri.rabade2022@gmail.com>
2023-04-18 12:06:18 +0530

    this is done by Shrikant

---
+++ src/main/java/com/BikkadITControllerToUI/WelcomeController.java
@@ -0,0 +1,5 @@
+package com.BikkadITControllerToUI;
+
+public class WelcomeController {
+
+}
---
+++ src/test/java/com/BikkadITControllerToUI/ControllerToUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadITControllerToUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerToUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
